c65c29b589f53e3e1bc6f8cef149231073aab422
Dead_Moroz <malussapiens@gmail.com>
2022-12-20 20:20:43 +0300

    Tasks updated

--- Seminar1/S1T2/s1t2.py
+++ Seminar1/S1T2/s1t2.py
@@ -10,8 +10,7 @@ print('  X', '  Y', '  Z' + '\t'*2+'¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z', se
 for x in [False, True]:
     for y in [False, True]:
         for z in [False, True]:
-            print(x, y, z)
-            #print(x, y, z, end='\t'*3+'    ', sep='\t')
+            print(x, y, z, end='\t'*3+'    ', sep='\t')
             p1 = not (x or y or z)
             p2 = not x and not y and not z
             print(p1 == p2)
--- Seminar2/S2T1/s2t1.py
+++ Seminar2/S2T1/s2t1.py
@@ -29,3 +29,25 @@ print(sum_digs)
 #     if i.isdigit():
 #         sum_digs += int(i)
 # print(sum_digs)
+
+# Способ 3Ж
+#Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+
+# n = input()
+# summ = 0
+# for i in n:
+#     if i.isdigit():
+#         summ+=int(i)
+# print(summ)
+
+# n = float(input())
+# len_num = len(str(n))-1
+# summ=0
+# while n!=int(n):
+#     n= round(n*10,len_num)
+#     print(n)
+# while n>0:
+#     summ+=n%10
+#     n = n//10
+# print(summ)
+
